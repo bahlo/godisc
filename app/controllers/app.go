1f578433a2f16579ef9f0437a6022976f319fa85
Arne Bahlo <hallo@arne.me>
2014-04-19 19:07:37 +0200

    Redirect index to login

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -1,11 +1,19 @@
 package controllers
 
-import "github.com/revel/revel"
+import (
+  "github.com/revel/revel"
+  "godisc/app/routes"
+)
 
 type App struct {
   *revel.Controller
 }
 
 func (c App) Index() revel.Result {
+  // TODO: Redirect to index if logged in
+  return c.Redirect(routes.App.ShowLogin())
+}
+
+func (c App) ShowLogin() revel.Result {
   return c.Render()
 }
