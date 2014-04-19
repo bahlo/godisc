898d3fb89c9a1966e3e227057cae24725165b0ec
Arne Bahlo <hallo@arne.me>
2014-04-19 19:16:29 +0200

    Add login check

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -9,11 +9,27 @@ type App struct {
   *revel.Controller
 }
 
+// Index
 func (c App) Index() revel.Result {
   // TODO: Redirect to index if logged in
   return c.Redirect(routes.App.ShowLogin())
 }
 
+// Show login form
 func (c App) ShowLogin() revel.Result {
   return c.Render()
+}
+
+func (c App) DoLogin(username, password string) revel.Result {
+  c.Validation.Required(username)
+  c.Validation.Required(password)
+  c.Validation.MinSize(username, 3)
+
+  if c.Validation.HasErrors() {
+    c.Validation.Keep()
+    c.FlashParams()
+    return c.Redirect(App.ShowLogin)
+  }
+
+  return c.Redirect(routes.App.Index())
 }
