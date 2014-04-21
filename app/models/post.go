c6a1fe07a9522361c64015df031469d9610d9b42
Arne Bahlo <hallo@arne.me>
2014-04-22 01:11:42 +0200

    Add user information to threads and posts

--- app/models/post.go
+++ app/models/post.go
@@ -1,5 +1,10 @@
 package models
 
+import (
+  "fmt"
+  "github.com/coopernurse/gorp"
+)
+
 type Post struct {
   PostId         int
   ThreadId       int
@@ -10,4 +15,36 @@ type Post struct {
   // Transient
   Thread         *Thread
   User           *User
+}
+
+func (b Post) String() string {
+  return fmt.Sprintf("Post(%s)", b.User)
+}
+
+func (b *Post) PreInsert(_ gorp.SqlExecutor) error {
+  b.ThreadId = b.Thread.ThreadId
+  b.UserId = b.User.UserId
+
+  return nil
+}
+
+func (b *Post) PostGet(exe gorp.SqlExecutor) error {
+  var (
+    obj interface{}
+    err error
+  )
+
+  obj, err = exe.Get(User{}, b.UserId)
+  if err != nil {
+    return fmt.Errorf("failed loading a posts's user (%d): %s", b.UserId, err)
+  }
+  b.User = obj.(*User)
+
+  obj, err = exe.Get(Thread{}, b.ThreadId)
+  if err != nil {
+    return fmt.Errorf("failed loading a posts's thread (%d): %s", b.ThreadId, err)
+  }
+  b.Thread = obj.(*Thread)
+
+  return nil
 }
