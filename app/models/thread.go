b6941a978c61e61e965f5c1ce4037bd2c0f684d9
Arne Bahlo <hallo@arne.me>
2014-04-22 01:04:57 +0200

    Add PreInsert and PostGet for Thread

--- app/models/thread.go
+++ app/models/thread.go
@@ -1,5 +1,10 @@
 package models
 
+import (
+  "fmt"
+  "github.com/coopernurse/gorp"
+)
+
 type Thread struct {
   ThreadId       int
   UserId         int
@@ -8,4 +13,28 @@ type Thread struct {
 
   // Transient
   User           *User
+}
+
+func (b Thread) String() string {
+  return fmt.Sprintf("Thread(%s)", b.User)
+}
+
+func (b *Thread) PreInsert(_ gorp.SqlExecutor) error {
+  b.UserId = b.User.UserId
+  return nil
+}
+
+func (b *Thread) PostGet(exe gorp.SqlExecutor) error {
+  var (
+    obj interface{}
+    err error
+  )
+
+  obj, err = exe.Get(User{}, b.UserId)
+  if err != nil {
+    return fmt.Errorf("Error loading a threads's user (%d): %s", b.UserId, err)
+  }
+  b.User = obj.(*User)
+
+  return nil
 }
